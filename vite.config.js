9be484ca84617572c5f6a0b5edc975647f35786d
afsalvp <161107544+afsalvp@users.noreply.github.com>
2025-04-20 21:01:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Movie-Watch-List/', // 👈 Add this line
   plugins: [react()],
 })
