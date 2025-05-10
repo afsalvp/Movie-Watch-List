8239a241a4bee928cb0b377d140daf36b43e717f
afsal vp <vpafsal333@example.com>
2025-05-10 20:03:11 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Movie-Watch-List/',  // This must match the repo name!
   plugins: [react()],
-})
+});
